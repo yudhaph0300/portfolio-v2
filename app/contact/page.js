9b709e9a662bb3846258dd3f282fe1f742a42a97
yudhaph0300 <yudhapamungkas0300@gmail.com>
2026-08-25 17:13:33 +0700

    add github and email

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import PillNav from "../../components/reusable/PillNav/PillNav";
 import FooterSection from "../../components/reusable/FooterSection/FooterSection";
 import styles from "./page.module.css";
@@ -46,6 +47,20 @@ export default function ContactPage() {
                      <small>Background &amp; experience</small>
                      Download CV
                   </span>
+               </a>
+            </div>
+
+            <div className={styles.contactLinks} aria-label="Additional contact links">
+               <a href="https://github.com/yudhaph0300" target="_blank" rel="noreferrer">
+                  <Image className={styles.contactIcon} src="/icon/github.svg" alt="" width={16} height={16} aria-hidden="true" />
+                  <span>GitHub</span>
+               </a>
+               <a href="mailto:yudhapamungkas0300@gmail.com">
+                  <svg className={styles.contactIcon} viewBox="0 0 24 24" aria-hidden="true">
+                     <path d="M3 5h18v14H3z" fill="none" stroke="currentColor" strokeWidth="1.7" />
+                     <path d="m4 7 8 6 8-6" fill="none" stroke="currentColor" strokeWidth="1.7" />
+                  </svg>
+                  <span>Email</span>
                </a>
             </div>
 
